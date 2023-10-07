82ef37789c14553863e303661d35d7ccbe6af48b
ChairGTables <chairgtables9@gmail.com>
2023-10-07 16:07:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import BlogEntry from './components/BlogEntry';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [blogEntries, setBlogEntries] = useState(JSON.parse(localStorage.getItem('ld-blog-entries') || '[]' ));
+    
+    const foo = 'foo';
+
+    const [selectedBlogEntry, setSelectedBlogEntry] = useState(-1);
+
+    
+    const [formData, setFormData] = useState({});
+
+    const addBlogEntry = () => {
+        let blogEntriesCopy = [...blogEntries, {
+            title: formData.title,
+            text: formData.text
+        }];
+
+        setFormData({});
+
+        setBlogEntries (blogEntriesCopy);
+        localStorage.setItem('ld-blog-entries', JSON.stringify(blogEntriesCopy));
+    }
+
+    const updateField = (field, value) => {
+        let formDataCopy = {...formData};
+        formDataCopy[field] = value;
+        setFormData(formDataCopy);
+    }
+
+    const formStyle = {display: "flex", flexDirection: "column", gap: "10px"};
+
+    return (
+        <div>
+            {blogEntries.map((blogEntry, index) => {
+                return (
+                        <BlogEntry onSelect={() => {setSelectedBlogEntry(index)}} selected={index === selectedBlogEntry} blogEntry={blogEntry} />
+                )
+            })}
+            <div style={formStyle}>
+                <label>Title</label>
+                <input type="text" value={formData.title}  onChange={(evt) => {updateField("title", evt.target.value)} }/>
+                <label>Text</label>
+                <textarea value={formData.text} onChange={(evt) => {updateField("text", evt.target.value)}}/>
+                <button onClick={addBlogEntry}  type="button">Submit</button>   
+            </div>
+        </div>
+    );
 }
 
 export default App;
